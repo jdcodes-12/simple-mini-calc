f4d9c5eca35ff4e95ac5278f2dc95a7b4e5dff6f
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-06-30 07:38:07 -0500

    Update Calc Ouput Window

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-prev-op]');
+const currentOperandTextElement = document.querySelector('[data-cur-op]');
+
+
+console.log(operationButtons);
+console.log(equalsButton);
+console.log(deleteButton);
+console.log(allClearButton);
+console.log(previousOperandTextElement);
+console.log(currentOperandTextElement);

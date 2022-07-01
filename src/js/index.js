1524db7ea54f1b7a1bb3ba21033d0c3cac3b27ad
Johnny Dickson <johnnydicksoncode@gmail.com>
2022-07-01 08:44:08 -0500

    Finish Calculator Logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,84 @@
-// Program Our Calc's Functionality (behavior)
+/* 
+  Follow Steps in Order:
+    1. Select Buttons on calculator UI via data attributed
+    2. Create `Calculator` class to code calculator logic (Constructor + all functions .. will be blank implementation)
+    3. Create a new `Calculator` object - enable us to use `Calculator` class
+    4. Add event listerners to number keys
+    5. Edit `UpdateDisplay`
+    6. Edit `AppendNumber(number)`
+    7. Add event listerns to operation keys
+    8. Edit `chooseOperation(operation)`
+    9. Add to `UpdateDisplay`
+    10. Edit `chooseOperation(operation)
+    11. Add Equals Button Event Listener
+    12. Edit `compute()`
+    13. Add All Clear Button Event Listener
+    14. Edit `delete()`
+    15. Add Delete Button Event Listener
+    16. Edit `UpdateDisplay()`
+    17. Edit `getDisplayNumber()`
+    18. Edit `UpdateDisplay()`
+
+*/
+
+/**  (1) Select Buttons On Calculator via data attributes**/
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation'); 
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const prevOpTextElement = document.querySelector('[data-prev-op]');
+const currOpTextElement = document.querySelector('[data-cur-op]');
+
+
+/** (2) Create Class for Calculator functionality - "The Logic" of the Calculator **/
 class Calculator {
 
-  constructor(previousOperandTextElement, currentOperandTextElement) {
-    this.previousOperandTextElement = previousOperandTextElement;
-    this.currentOperandTextElement = currentOperandTextElement;
-    this.clear();
+  // Run this on setup - put new calculator objects in intial state
+  constructor(prevOpTextElement, currOpTextElement) {
+    this.prevOpTextElement = prevOpTextElement;
+    this.currOpTextElement = currOpTextElement;
+    this.clear(); 
   }
 
-  // methods/functions
+  // clear all entered numbers
   clear() {
     this.currentOperand = '';
     this.previousOperand = '';
     this.operation = undefined;
   }
 
+  // Remove one digit off curr number
   delete() {
-    this.currentOperand = this.chooseOperation.toString().slice(0, -1);
+
+     // (14) 
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
 
+  // display selected number on the calc
   appendNumber(number) {
-    if (number === '.' && this.currentOperand.includes('.')) {
+
+    // (6) Stop if multiple '.' are clicked. Limits to 1 in window. 
+    if( number === '.' && this.currentOperand.includes('.') ) {
       return;
     }
 
-    this.currentOperand = this.currentOperand.toString() +
-                          number.toString();
+    // (6) display new number in screen @ end
+    this.currentOperand = this.currentOperand.toString() + number.toString();
   }
 
+  // (8) Chooose operation, clear the window display, 
+  //     and put into the prevOp slot in window
   chooseOperation(operation) {
 
-    // If current op is empty, skip
-    if (this.currentOperand === '') {
+    // (10) If no op selected, return 
+    if ( this.currentOperand === '' ) {
       return;
     }
 
-    // If not empty string (something in bot), do computation
-    if (this.previousOperand !== '') {
-      this.compute();
+    // (10) compute a value if there are operands
+    if ( this.previousOperand !== '' ) {
+      this.compute(); 
     }
 
     this.operation = operation;
@@ -44,15 +86,17 @@ class Calculator {
     this.currentOperand = '';
   }
 
+  // compute a single value in window
   compute() {
+
+    // (12) add code logic
     let computation;
-    
-    // get values
     const prev = parseFloat(this.previousOperand);
     const current = parseFloat(this.currentOperand);
-
-    // Cancel computation if NaN
-    if ( isNaN(prev) || isNaN(current) ) {
+    
+    // Check to see there are numbers in prev & cur before
+    // doing calculation
+    if ( isNaN(prev) && isNaN(current) ) {
       return;
     }
 
@@ -60,17 +104,20 @@ class Calculator {
       case '+': 
         computation = prev + current;
         break;
+      
       case '-':
         computation = prev - current;
         break;
       case '*':
         computation = prev * current;
         break;
+
       case '/':
         computation = prev / current;
         break;
-      default: 
-        return;
+     
+      default:
+          return; 
     }
 
     this.currentOperand = computation;
@@ -78,63 +125,96 @@ class Calculator {
     this.previousOperand = '';
   }
 
+  // Work with Decimal Numbers
   getDisplayNumber(number) {
-    return number;
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split('.')[0]); // get numbers
+    const decimalDigits = stringNumber.split('.')[1];  // get decimal 
+    let integerDisplay;
+
+    // return if NaN on intg
+    if ( isNaN(integerDigits) ) {
+      integerDisplay = ''
+    } 
+
+    // else convert integer display
+    else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+    }
+
+    // Check for decimal digits, non-null means a decimal numer was entered
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } 
+
+    else {
+      return integerDisplay;
+    }
   }
 
+  // update calc window display
   updateDisplay() {
-    this.currentOperandTextElement.innerText =
-      this.getDisplayNumber(this.currentOperand);
 
-    // display operation symbol in display
-    if(this.operation != null){
-      this.previousOperandTextElement.innerText = 
-        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+    this.currOpTextElement.innerText =
+      this.getDisplayNumber(this.currentOperand)
+    if (this.operation != null) {
+      this.prevOpTextElement.innerText =
+        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    } 
+
+    // (18) edit formatting for prev op
+    else {
+      this.prevOpTextElement.innerText = ''
     }
   }
 }
 
-// Select Buttons on Calculator
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButton = document.querySelector('[data-equals]');
-const deleteButton = document.querySelector('[data-delete]');
-const allClearButton = document.querySelector('[data-all-clear]');
-const previousOperandTextElement = document.querySelector('[data-prev-op]');
-const currentOperandTextElement = document.querySelector('[data-cur-op]');
+// (3) Create new Calc Object 
+const calculator = new Calculator(prevOpTextElement, currOpTextElement);
 
-// create a calculator object
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
-// Give Number Keys Functionality
-numberButtons.forEach(button => {
+/** Give buttons functionality via event listeners **/
+
+// (4) Functionality: Show number values in display window (e.g. 0-9 & '.')
+numberButtons.forEach( button => {
+
   button.addEventListener('click', () => {
     calculator.appendNumber(button.innerText);
     calculator.updateDisplay();
   });
+
 });
 
-// Give Operation Keys Functionality
-operationButtons.forEach(button => {
+// (7) Functionality: Select the right operation to use
+operationButtons.forEach( button => {
+
   button.addEventListener('click', () => {
     calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   });
+
 });
 
-// Give Equals Button Functionality
+// (11) Functionality: Equals Button
 equalsButton.addEventListener('click', button => {
+
   calculator.compute();
   calculator.updateDisplay();
+
 });
 
-// Give AC Button Functionality
+// (13) Functionality: clear window
 allClearButton.addEventListener('click', button => {
+
   calculator.clear();
   calculator.updateDisplay();
+
 });
 
+// (15) Functionality: delete 1 # from current window
 deleteButton.addEventListener('click', button => {
+
   calculator.delete();
   calculator.updateDisplay();
+
 });
